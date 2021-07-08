409732a35e3beae715eafe5f819355576dc5d4ba
omgwer <hersgory2134@gmail.com>
2021-07-08 10:59:01 +0300

    dd

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,7 +3,7 @@ window.addEventListener('load', evt => {
   accordionList();
   menuScrollNavigation();
   scrollToStartPage();
-  customDropList();
+  //customDropList();
   feedbackSlider();
   initBurgerMenu();
   });
@@ -62,22 +62,22 @@ function accordionList() {
   });
 }
 
-function customDropList() {
-  const selected = document.querySelector(".selected");
-  const optionsContainer = document.querySelector(".options-container");
-  const optionsList = document.querySelectorAll(".option");
-
-  selected.addEventListener("click", () => {
-    optionsContainer.classList.toggle("active");
-  });
-
-  optionsList.forEach(option => {
-    option.addEventListener("click", () => {
-      selected.innerHTML = option.querySelector("label").innerHTML;
-      optionsContainer.classList.remove("active");
-    });
-  });
-}
+// function customDropList() {
+//   const selected = document.querySelector(".selected");
+//   const optionsContainer = document.querySelector(".options-container");
+//   const optionsList = document.querySelectorAll(".option");
+//
+//   selected.addEventListener("click", () => {
+//     optionsContainer.classList.toggle("active");
+//   });
+//
+//   optionsList.forEach(option => {
+//     option.addEventListener("click", () => {
+//       selected.innerHTML = option.querySelector("label").innerHTML;
+//       optionsContainer.classList.remove("active");
+//     });
+//   });
+// }
 
 function scrollToStartPage() {
   const button = document.getElementById('scrollToStart');
